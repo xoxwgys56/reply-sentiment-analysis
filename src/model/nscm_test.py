9c0b8d32a8cc6e08f861ea7996b81ff08baa4d7a
xdfc1745 <39234312+xdfc1745@users.noreply.github.com>
2021-07-15 16:50:35 +0900

    Update nscm_test.py
    add comment

--- src/model/nscm_test.py
+++ src/model/nscm_test.py
@@ -13,7 +13,8 @@ tokenizer = AutoTokenizer.from_pretrained(
 )
 
 
-def getitem(text):
+def getitem(text): 
+# input text make in model input(tokenize, make attention mask)
 
     inputs = tokenizer(
         text,
@@ -31,19 +32,22 @@ def getitem(text):
 
 
 def test():
+    # get a pre-trained model from Transformer 
     model = ElectraForSequenceClassification.from_pretrained(
         "monologg/koelectra-small-v2-discriminator"
     )
 
     try:
+        # load trained model 
         model.load_state_dict(torch.load("model.pt"))
     except:
+        # if not have trained model, first train in train code
         pass
 
     batch_size = 128
     optimizer = AdamW(model.parameters(), lr=1e-5)
 
-    while True:
+    while True: # predict sentiment
         logger.info('please input reply >')
         reply = input()
         if reply == 'quit':
